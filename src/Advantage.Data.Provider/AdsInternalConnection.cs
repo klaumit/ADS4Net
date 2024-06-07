d573b7b14b708b96fed4e0fd0cebb1e585d6d493
Klaus Mittermann <klaus.mittermann@test.com.uk>
2024-06-07 22:00:20 +0200

    Adding pool

---
+++ src/Advantage.Data.Provider/AdsInternalConnection.cs
@@ -0,0 +1,6 @@
+namespace Advantage.Data.Provider
+{
+    public class AdsInternalConnection
+    {
+    }
+}
---
+++ src/Advantage.Data.Provider/AdsPooledInternalConnection.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Advantage.Data.Provider
+{
+    internal class AdsPooledInternalConnection : AdsInternalConnection
+    {
+        private AdsConnectionPool mPool;
+        private DateTime mCreateTime;
+
+        public AdsPooledInternalConnection() => throw new NotSupportedException();
+
+        public AdsPooledInternalConnection(
+            string strConnectionString,
+            AdsConnectionStringHandler handler,
+            AdsConnectionPool pool)
+            : base(strConnectionString, handler)
+        {
+            this.mPool = pool;
+            this.mCreateTime = DateTime.Now;
+        }
+
+        public static void ReturnConnectionToPool(AdsPooledInternalConnection pooledConnection)
+        {
+            pooledConnection.ConnectionPool.ReturnConnectionToPool(pooledConnection);
+        }
+
+        public void DecrementPoolOpenCount()
+        {
+            if (this.mbDisposed || this.mPool == null)
+                return;
+            this.mPool.DecrementOpenCount();
+        }
+
+        public AdsConnectionPool ConnectionPool => this.mPool;
+
+        public DateTime CreationTime => this.mCreateTime;
+    }
+}
