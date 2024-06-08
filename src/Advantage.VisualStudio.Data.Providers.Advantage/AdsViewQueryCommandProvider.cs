c68bccf71c7c453778b192edcc48ca922f59e6ee
Klaus Mittermann <klaus.mittermann@test.com.uk>
2024-06-08 02:34:49 +0200

    Removing old code

--- src/Advantage.VisualStudio.Data.Providers.Advantage/AdsViewQueryCommandProvider.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.VisualStudio.Data.Framework;
-
-namespace Advantage.VisualStudio.Data.Providers.Advantage
-{
-    internal class AdsViewQueryCommandProvider : DataViewCommandProvider
-    {
-    }
-}
