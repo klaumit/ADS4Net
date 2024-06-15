7249e0b1db178c4b25cb9d1cbe80a56975593db2
Klaus Mittermann <klaus.mittermann@test.com.uk>
2024-06-15 20:51:48 +0200

    Adding small test con

---
+++ test/SmallConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Advantage.Data.Provider.Test;
+
+namespace SmallConsole
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var source = AdsHelper.GetSource("hello");
+            var connStr = AdsHelper.GetConnStr(source, isLocal: true);
+
+            using var conn = AdsHelper.GetConn(connStr);
+            conn.Open();
+
+            Console.WriteLine(conn);
+            Console.WriteLine(conn.State);
+        }
+    }
+}
